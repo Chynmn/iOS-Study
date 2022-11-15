cf1790d115b42e189a78bccc4c43e451593a9561
Chynmn <chynmn0523@gmail.com>
2022-11-15 18:06:52 +0900

    Create Project Ch23 Obs_Env_Tutorial

---
+++ HyunMin/Obs_Env_Tutorial/Obs_Env_Tutorial/Obs_Env_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Obs_Env_TutorialApp.swift
+//  Obs_Env_Tutorial
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct Obs_Env_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
