24ffebf07f7b2eadecd9bd906d70e209661a20fd
Jeong Dong <eejxdong99@gmail.com>
2022-11-15 15:05:23 +0900

    ch26 init

---
+++ JeongDong/ListNavDemo/ListNavDemo/ListNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavDemoApp.swift
+//  ListNavDemo
+//
+//  Created by 이정동 on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct ListNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
